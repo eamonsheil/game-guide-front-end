79197006d4546317374e74f6fa1398b0d30453a9
Eamon Sheil <Eamondsheil@gmail.com>
2022-04-18 17:16:13 -0400

    added main components

---
+++ src/components/GameDetail.js
@@ -0,0 +1,23 @@
+
+
+
+
+function GameDetail({currentGame}) {
+
+
+    return(
+
+            <div>
+               <img src={currentGame.image_url} alt={currentGame.title} height="100px" width="100px"/>
+               <p>Title: {currentGame.title}</p>
+               <p>Playstyle: {currentGame.play_style}</p>
+               <p>Rating: {Math.floor(currentGame.rating)}</p>
+               <p>Average play time: {currentGame.avg_play_time}</p>
+               <p>Min/Max players: {currentGame.min_players}/{currentGame.max_players}</p>
+               <p>{currentGame.description}</p>
+            </div>
+
+    )
+}
+
+export default GameDetail
