54d620205b7b5a8b96e99bf64a9fe12769ad2112
Eamon Sheil <Eamondsheil@gmail.com>
2022-04-18 13:54:23 -0400

    1st commit

---
+++ src/components/GamesList.js
@@ -0,0 +1,17 @@
+
+
+function GamesList() {
+
+    return (
+        <>
+        <h3>This will be the gameslist</h3>
+        <p>game</p>
+        <p>game</p>
+        <p>game</p>
+        <p>game</p>
+        
+        </>
+    )
+}
+
+export default GamesList;
---
+++ src/components/UserPage.js
@@ -0,0 +1,10 @@
+
+function UserPage() {
+
+    return(
+
+        <h2>User Page....</h2>
+    )
+}
+
+export default UserPage
