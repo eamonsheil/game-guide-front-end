2ddbaba84be34df73a3e850d97e390caeab50299
Calypso Leonard <calypsorleonard@gmail.com>
2022-04-19 17:52:34 -0400

    still making context a thing

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,19 +9,27 @@ function Home() {
     const [user, setUser] = useContext(UserContext)
 
     const [userInfo, setUserInfo] = useState(defaultobj)
-    const [testUser, setTestUser] = useState({
-        username: "",
-        password: ""
-    })
+    const [testUser, setTestUser] = useState(defaultobj)
     const [isNewUser, setIsNewUser] = useState(false)
     const navigate = useNavigate()
 
     function handleSubmit(e) {
         e.preventDefault()
+        console.log(userInfo)
+
+        // there's something up with state here ugh
 
         fetch(`http://localhost:9292/users/by-username/${userInfo.username}`)
         .then( res => res.json())
-        .then( data => setTestUser(data))
+        .then( data => {
+            console.log(data)
+            if (data){
+                console.log("there's data!")
+                setTestUser({user: data.username, password:data.password})
+                console.log(testUser)
+            }
+        })
+
 
         if (userInfo.username === testUser.username && userInfo.password === testUser.username){
             setUser(testUser)
@@ -34,7 +42,6 @@ function Home() {
     }   
 
     function handleChange({target: {value, name}}) {
-        // const {target: {value, name}} = e
         setUserInfo({...userInfo, [name]: value})
     }
 
@@ -49,6 +56,8 @@ function Home() {
                 <input type="text" name="password" onChange={handleChange} value={userInfo.password}/>
                 <input type="submit"/>
             </form>
+
+            <button>continue as guest</button>
 
             {isNewUser ? <NewUserForm setIsNewUser={setIsNewUser} isNewUser={isNewUser}/> : null}
 
