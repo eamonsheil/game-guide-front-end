125b26a4237841d84473a2d4acad38ba4796de10
Calypso Leonard <calypsorleonard@gmail.com>
2022-04-19 14:34:53 -0400

    added to search form

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -4,17 +4,24 @@
 
 function GameDetail({currentGame}) {
 
+    function getPrettyComments {
+        currentGame.game_relationships
+    }
+
 
     return(
 
             <div>
-               <img src={currentGame.image_url} alt={currentGame.title} height="100px" width="100px"/>
-               <p>Title: {currentGame.title}</p>
-               <p>Playstyle: {currentGame.play_style}</p>
-               <p>Rating: {Math.floor(currentGame.rating)}</p>
-               <p>Average play time: {currentGame.avg_play_time}</p>
-               <p>Min/Max players: {currentGame.min_players}/{currentGame.max_players}</p>
-               <p>{currentGame.description}</p>
+                <img src={currentGame.image_url} alt={currentGame.title} height="200px" width="auto"/>
+                <p><strong>Title: </strong>{currentGame.title}</p>
+                <p><strong>Categories:</strong> {currentGame.categories.split("_").join(" ")}</p>
+                <p><strong>Rating: </strong>{Math.floor(currentGame.rating)} / 10</p>
+                <p><strong>Play time:</strong> {currentGame.min_play_time} - {currentGame.max_play_time} minutes</p>
+                <p><strong>Number of players:</strong> {currentGame.min_players} to {currentGame.max_players}</p>
+                {currentGame.description}
+                <div className="comments">
+                 {/* currentGame.game_relationships.comment */}
+                </div>
             </div>
 
     )
--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -1,52 +1,102 @@
 import {useState, useEffect} from 'react'
 import GameDetail from './GameDetail'
+// import ReactHtmlParser from 'react-html-parser'
 
 function GamesList() {
     const [games, setGames] = useState([])
-    const [search, setSearch] = useState("")
+    const [formData, setFormData] = useState({
+        search:  "",
+        numPlayers: 0,
+        playtime: 0,
+        category: "all"
+    })
+    //const [search, setSearch] = useState("")
     const [showDetail, setShowDetail] = useState(false)
     const [currentGame, setCurrentGame] = useState({})
 
     useEffect(() => {
-        fetch(`http://localhost:9292/games`)
+        fetch(`http://localhost:9292/games_with_comments`)
         .then( res => res.json())
         .then( data => setGames(data))
     },[])
 
     const showGames = games.map((game) => {
+        const description = document.createElement("div")
+        console.log(game.description)
+                    //description.innerHTML = game.description
         return(
-            <li>
-               <img src={game.image_url} alt={game.title} height="100px" width="100px"/>
-               <p>Title: {game.title}</p>
-               <p>{game.description}</p>
-                <button onClick={() => toggleGameDetail(game)}>View Details</button>
-            </li>
+            <div className='game-list'>
+                <ul>
+                    <img className="games-list-img" src={game.image_url} alt={game.title} height="100px" width="100px"/>
+                    <p>Title: <strong>{game.title}</strong></p>
+
+                    {/* {ReactHtmlParser(game.description)} */}
+                    {/* <div style="height:120px;width:120px;border:1px solid #ccc;font:16px/26px Georgia, Garamond, Serif;overflow:auto;"> */}
+                    {/* </div> */}
+                    <button onClick={() => toggleGameDetail(game)}>View Details</button>
+                </ul>
+            </div>
         )
     })
 
     function toggleGameDetail(game) {
-        console.log(currentGame, showDetail)
+        //console.log(currentGame, showDetail)
         setShowDetail(!showDetail)
         setCurrentGame(game)
     }
 
+    function handleFormChange(event){
+        setFormData({
+            ...formData,
+            [event.target.name] : event.target.value
+        })
+    }
+
+    function handleFormSubmit(event){
+        event.preventDefault()
+        fetch(`http://localhost:9292/games/${formData.search}/${formData.playtime}/${formData.numPlayers}`)
+        .then( res => res.json())
+        .then( data => setGames(data))
+        .catch( error => console.log(error.message));
+    }
+
     return (
         <>
-         <h3>This will be the gameslist</h3>
-         <form>
-            {/* <input name="search" placeholder='Search for games....' value={search} onChange={search}/> */}
+        <h3>Welcome to Game Guide</h3>
+        <form onSubmit={handleFormSubmit}>
+            <input name="search" placeholder='Search by name....' value={formData.search} onChange={handleFormChange}/>
             <br/>
-            <select>
-                <option value="all">Rating</option>
-                <option></option>
-                <option></option>
-                <option></option>
+            <select onChange={handleFormChange}>
+                <option value="all">Category </option>
+                <option>Card Game</option>
+                <option>Dice Game</option>
+                <option>Negotiation Game</option>
             </select>
+            <label>Num players:
+                <input 
+                    onChange={handleFormChange} 
+                    //value={formData.numPlayers} 
+                    placeholder=""
+                    min="0"
+                    name="min_players" 
+                    type="number"/>
+            </label>
+            <label>Available play time (in min):
+                <input 
+                    onChange={handleFormChange} 
+                    //value={formData.numPlayers} 
+                    placeholder=""
+                    min="0"
+                    name="max_players" 
+                    type="number"/>
+            </label>
+            <input type="submit"></input>
+            </form>
 
-         </form>
+            
         
         <ul>
-        {showDetail? <button onClick={() => setShowDetail(!showDetail)}>Show All</button> : null}
+            {showDetail ? <button onClick={() => setShowDetail(!showDetail)}>Show All</button> : null}
             {showDetail ? <GameDetail currentGame={currentGame}/> : showGames}
         </ul>
         
