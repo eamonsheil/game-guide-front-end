e516c29ebca126fbd02b21aa70c88f2b4df00477
Eamon Sheil <Eamondsheil@gmail.com>
2022-04-20 14:43:16 -0400

    game detail work

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -3,6 +3,7 @@ import Header from './Header'
 
 function GameDetail({currentGame}) {
 
+    console.log(currentGame)
     function getPrettyComments(){
         const comments = currentGame.game_relationships.map(relationship => {
             return <p>{relationship.comment + " - "}  <strong>{relationship.user.username} </strong></p>
@@ -14,13 +15,16 @@ function GameDetail({currentGame}) {
     return(
         <div>
                 <Header />
-                <img src={currentGame.image_url} alt={currentGame.title} height="200px" width="auto"/>
-                <p><strong>Title: </strong>{currentGame.title}</p>
-                <p><strong>Categories:</strong> {currentGame.categories.split("_").join(" ")}</p>
-                <p><strong>Rating: </strong>{Math.floor(currentGame.rating)} / 10</p>
-                <p><strong>Play time:</strong> {currentGame.min_play_time} - {currentGame.max_play_time} minutes</p>
-                <p><strong>Number of players:</strong> {currentGame.min_players} to {currentGame.max_players}</p>
-                {currentGame.description}
+                <img class="game-detail-img" src={currentGame.image_url} alt={currentGame.title} height="200px" width="auto"/>
+                
+                <div class="game-detail-info">
+                    <p><strong>Title: </strong>{currentGame.title}</p>
+                    <p><strong>Categories:</strong> {currentGame.categories.split("_").join(" ")}</p>
+                    <p><strong>Rating: </strong>{Math.floor(currentGame.rating)} / 10</p>
+                    <p><strong>Play time:</strong> {currentGame.min_play_time} - {currentGame.max_play_time} minutes</p>
+                    <p><strong>Number of players:</strong> {currentGame.min_players} to {currentGame.max_players}</p>
+                    {currentGame.description}
+                </div>
                 <div className="comments">
                     <strong>User Comments:</strong>
                     <div>
